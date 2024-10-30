cd5de71ee2f3fd6633f7b071b0704ebfc0720696
42null <averyparanoiduser@gmail.com>
2024-10-30 13:59:02 -0500

    Better labels and new image

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		int weight, heightFeet = 0, heightInches = 0;
+		if(int.TryParse(Inp_weight.Text, out weight) && (int.TryParse(Inp_height_feet.Text, out heightFeet) | int.TryParse(Inp_height_inches.Text, out heightInches))){
+			double bmi = weight / Math.Pow(heightFeet * 12 + heightInches, 2) * 703;
+			Lbl_BMI.Text = $"Your BMI is {bmi}";
+		}
+
+		// ((Button) sender).Text = $"You clicked {count} times.";
+
+		// if (count == 1)
+		// 	OutText.Text = $"Clicked {count} time";
+		// else
+		// 	OutText.Text = $"Clicked {count} times";
+
+
+		// // SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(Lbl_BMI.Text);
+		
+	}
+}
+
